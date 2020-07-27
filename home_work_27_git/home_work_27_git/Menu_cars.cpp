9d30857cae119622411554275eae5780b923da00
Василь Колотюк <vasakolotuk02@gmail.com>
2020-07-27 10:45:56 +0300

    add four methods for outputting information about cars from the showroom such as, output of all cars, output by name, output by price, output by date of issue

--- home_work_27_git/home_work_27_git/Menu_cars.cpp
+++ home_work_27_git/home_work_27_git/Menu_cars.cpp
@@ -27,3 +27,35 @@ void  Menu_cars::Delete_all_cars_manufactured_before_2000()
 {
 	cl.Delete_all_cars_manufactured_before_2000();
 }
+
+void Menu_cars::Show_all_cars()
+{
+	cl.Show_all_cars();
+}
+void Menu_cars::Show_by_name_car()
+{
+	string name;
+	cout << "Enter name car" << endl;
+	cout << "->";
+	cin >> name;
+	cl.Show_by_name_car(name);
+
+}
+
+void Menu_cars::Show_by_year()
+{
+	int year_of_issue;
+	cout << "Enter year of issue" << endl;
+	cout << "->";
+	cin >> year_of_issue;
+	cl.Show_by_year(year_of_issue);
+}
+
+void Menu_cars::Show_by_price()
+{
+	int price;
+	cout << "Enter price" << endl;
+	cout << "->";
+	cin >> price;
+	cl.Show_by_price(price);
+}
--- home_work_27_git/home_work_27_git/Menu_cars.h
+++ home_work_27_git/home_work_27_git/Menu_cars.h
@@ -9,5 +9,13 @@ private:
 	void Add_car();
 
 	void Delete_all_cars_manufactured_before_2000();
+
+	void Show_all_cars();
+
+	void Show_by_name_car();
+
+	void Show_by_year();
+
+	void Show_by_price();
 };
 
